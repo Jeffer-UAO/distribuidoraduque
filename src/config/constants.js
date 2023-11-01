b8aed6f8b88206f22abb626e3168cf07b4c2f45c
JefferDevop <somadesarrollo@gmail.com>
2023-11-01 06:07:49 -0500

    first commit

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API=NEXT_PUBLIC_API 
+export const BASE_API="https://distribucionesduque.catalogointeractivo.com.co"
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
 export const BASE_NAME=NEXT_PUBLIC_STORAGE 
