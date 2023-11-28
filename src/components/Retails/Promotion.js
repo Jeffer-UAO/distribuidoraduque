2d3192ed8ef893cd3b0c44d4a58d78343af02aef
JefferDevop <somadesarrollo@gmail.com>
2023-11-28 18:09:25 -0500

    Productos en oferta

--- src/components/Retails/Promotion.js
+++ src/components/Retails/Promotion.js
@@ -6,9 +6,13 @@ import { map } from "lodash";
 
 export function Promotion(props) {
   const { products } = props;
+
+  const scale = "c_scale,f_auto,q_50,w_400/";
+  const upload = "image/upload/";
+
   return (
     <div className={styles.content}>
-      <h5>Promociones</h5>
+      <h2>Promociones</h2>
 
       <div className={styles.list}>
         {map(
@@ -24,8 +28,14 @@ export function Promotion(props) {
                   >
                     <CardImg
                       alt="Card image cap"
-                      src={BASE_NAME + product.images}
+                      src={
+                        BASE_NAME +
+                        upload +
+                        scale +
+                        product.images.split(upload)[1]
+                      }
                     />
+
                     <div className={styles.product}>
                       <CardTitle className={styles.title}>
                         <h5>
@@ -41,10 +51,7 @@ export function Promotion(props) {
                     href={`/${product.slug}`}
                     className={styles.list__product}
                   >
-                    <CardImg
-                      alt="Card image cap"
-                      src={product.image_alterna}
-                    />
+                    <CardImg alt="Card image cap" src={product.image_alterna} />
                     <div className={styles.product}>
                       <CardTitle className={styles.title}>
                         <h5>
